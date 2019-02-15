951feb1180ccd1a48aebf344a9cca27e7c2ddb99
Lubna Rasheed (lrasheed) <lrasheed@cisco.com>
2019-02-15 18:49:40 -0500

    added links for docs

---
+++ docs/links.py
@@ -0,0 +1,30 @@
+
+internal_links = {'abstract': ('%s://wwwin-pyats.cisco.com/cisco-shared/abstract/html/', 'abstract'),
+                  'codereview': ('%s://bitbucket-eng-sjc1.cisco.com/bitbucket/projects/PYATS-PYPI/repos/genieparser/pull-requests?create', 'code review'),
+                  'pyats': ('%s://wwwin-pyats.cisco.com', 'pyATS'),
+                  'staticscope': ('%s://docs.python.org/3/whatsnew/2.1.html', 'static scope'),
+                  'devnet': ('%s://developer.cisco.com/', 'Cisco DevNet'),
+                  'mailto': ('mailto:asg-genie-support@%s','mailto'),
+                  'communityforum': ('%s://piestack.cisco.com', 'community forum'),
+                  'parsergen': ('%s://wwwin-pyats.cisco.com/cisco-shared/parsergen/latest/index.html', 'parsergen'),
+                  'classtutorial': ('%s://docs.python.org/3.4/tutorial/classes.html', 'Class tutorial'),
+                  'inheritancetutorial': ('%s://docs.python.org/2/tutorial/classes.html?highlight=inheritance', 'Inheritance Tutorial'),
+                  'parsers': ('%s://wwwin-pyats.cisco.com/cisco-shared/genie/latest/genie_libs/#/verifications', 'parsers'),
+                  'schemaengine': ('%s://wwwin-pyats.cisco.com/cisco-shared/metaparser/latest/advanced/schemaengine.html?highlight=schema', 'schema engine documentation'),
+                  }
+
+external_links = {'abstract': ('%ss://pubhub.devnetcloud.com/media/pyats-packages/docs/abstract/index.html', 'abstract'),
+                  'codereview': ('%ss://github.com/CiscoTestAutomation/genieparser/compare?expand=1', 'code review'),
+                  'pyats': ('%ss://developer.cisco.com/site/pyats/', 'pyATS'),
+                  'staticscope': ('%ss://docs.python.org/3/whatsnew/2.1.html', 'static scope'),
+                  'devnet': ('%ss://developer.cisco.com/', 'Cisco DevNet'),
+                  'mailto': ('mailto:pyats-support-ext@%s','mailto'),
+                  'communityforum': ('%ss://communities.cisco.com/community/developer/pyats', 'community forum'),
+                  'parsergen': ('%ss://pubhub.devnetcloud.com/media/pyats-packages/docs/parsergen/index.html', 'parsergen'),
+                  'classtutorial': ('%ss://docs.python.org/3.4/tutorial/classes.html', 'Class Tutorial'),
+                  'inheritancetutorial': ('%ss://docs.python.org/2/tutorial/classes.html?highlight=inheritance', 'Inheritance Tutorial'),
+                  'parsers': ('%ss://pubhub.devnetcloud.com/media/pyats-packages/docs/genie/genie_libs/#/verifications', 'parsers'),
+                  'schemaengine': ('%ss://pubhub.devnetcloud.com/media/pyats-packages/docs/metaparser/advanced/schemaengine.html?highlight=schema', 'schema engine documentation'),
+                  }
+
+
