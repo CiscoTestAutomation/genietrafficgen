ebb3b869297e5b8f0b8cf3f51e0c0136a2107c0c
Lubna Rasheed <lrasheed@cisco.com>
2019-03-05 11:55:14 -0500

    changes to README.md and setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-#! /usr/bin/env python
+#! /bin/env python
 
-'''Setup file for genie.trafficgen Namespace Package
+'''Setup file for Libs
 
 See:
     https://packaging.python.org/en/latest/distributing.html
 '''
 
 import os
-from ciscodistutils import setup, find_packages, is_devnet_build
-from ciscodistutils.tools import (read,
-                                  version_info,
-                                  generate_cython_modules)
+import re
+import sys
+
+from setuptools import setup, find_packages
 
 _INTERNAL_SUPPORT = 'asg-genie-support@cisco.com'
 _EXTERNAL_SUPPORT = 'pyats-support-ext@cisco.com'
@@ -18,36 +18,61 @@ _EXTERNAL_SUPPORT = 'pyats-support-ext@cisco.com'
 _INTERNAL_LICENSE = 'Cisco Systems, Inc. Cisco Confidential',
 _EXTERNAL_LICENSE = 'Apache 2.0'
 
-_INTERNAL_URL = 'http://wwwin-pyats.cisco.com/cisco-shared/genietrafficgen/html/'
+_INTERNAL_URL = 'http://wwwin-genie.cisco.com/'
 _EXTERNAL_URL = 'https://developer.cisco.com/site/pyats/'
 
+DEVNET_CMDLINE_OPT = '--devnet'
+devnet = False
+if DEVNET_CMDLINE_OPT in sys.argv:
+    # avoiding argparse complexity :o
+    sys.argv.remove(DEVNET_CMDLINE_OPT)
+    devnet = True
 
 # pyats support mailer
-SUPPORT = _EXTERNAL_SUPPORT if is_devnet_build() else _INTERNAL_SUPPORT
+SUPPORT = _EXTERNAL_SUPPORT if devnet else _INTERNAL_SUPPORT
 
 # license statement
-LICENSE = _EXTERNAL_LICENSE if is_devnet_build() else _INTERNAL_LICENSE
+LICENSE = _EXTERNAL_LICENSE if devnet else _INTERNAL_LICENSE
 
 # project url
-URL = _EXTERNAL_URL if is_devnet_build() else _INTERNAL_URL
+URL = _EXTERNAL_URL if devnet else _INTERNAL_URL
 
-# get version information
+def read(*paths):
+    '''read and return txt content of file'''
+    with open(os.path.join(*paths)) as fp:
+        return fp.read()
+
+def find_version(*paths):
+    '''reads a file and returns the defined __version__ value'''
+    version_match = re.search(r"^__version__ ?= ?['\"]([^'\"]*)['\"]",
+                              read(*paths), re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+def build_version_range(version):
+    '''
+    for any given version, return the major.minor version requirement range
+    eg: for version '3.4.7', return '>=3.4.0, <3.5.0'
+    '''
+    req_ver = version.split('.')
+    version_range = '>= %s.%s.0, < %s.%s.0' % \
+        (req_ver[0], req_ver[1], req_ver[0], int(req_ver[1])+1)
+
+    return version_range
+
+def version_info(*paths):
+    '''returns the result of find_version() and build_version_range() tuple'''
+
+    version = find_version(*paths)
+    return version, build_version_range(version)
+
+# compute version range
 version, version_range = version_info('src', 'genie', 'trafficgen', '__init__.py')
 
-install_requires=['setuptools', 'wheel', 'genie']
+# generate package dependencies
+install_requires=['setuptools', 'wheel', 'genie', 'ixnetwork']
 
-if is_devnet_build():
-    install_requires.insert(0, 'pyats')
-    install_requires.insert(0, 'ixnetwork')
-
-def find_examples(*paths):
-    '''finds all example files'''
-    files = []
-
-    for (dirpath, dirnames, filenames) in os.walk(os.path.join(*paths)):
-        files.append((dirpath, [os.path.join(dirpath, f) for f in filenames]))
-
-    return files
 
 # launch setup
 setup(
@@ -55,7 +80,7 @@ setup(
     version = version,
 
     # descriptions
-    description = 'Genie Library for traffic generator connection support',
+    description = 'Genie library for traffic generator devices connection support',
     long_description = read('DESCRIPTION.rst'),
 
     # the project's main homepage.
@@ -69,7 +94,7 @@ setup(
     license = LICENSE,
 
     # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -86,9 +111,8 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 
-
     # project keywords
-    keywords = 'genie traffic pyats cisco',
+    keywords = 'genie pyats test automation traffic',
 
     # uses namespace package
     namespace_packages = ['genie'],
@@ -104,13 +128,8 @@ setup(
     # additional package data files that goes into the package itself
     package_data = {},
 
-    # custom argument specifying the list of cythonized modules
-    cisco_cythonized_modules = generate_cython_modules('src/'),
-
     # console entry point
     entry_points = {
-        'pyats.utils.__legacy_imports__':
-            'trafficgen = genie.trafficgen:_IMPORTMAP',
     },
 
     # package dependencies
@@ -123,8 +142,7 @@ setup(
                 'restview',
                 'Sphinx',
                 'sphinxcontrib-napoleon',
-                'sphinx-rtd-theme',
-                'sphinxcontrib-mockautodoc'],
+                'sphinx-rtd-theme'],
     },
 
     # external modules
@@ -135,7 +153,7 @@ setup(
     # format:
     #   [('target', ['list', 'of', 'files'])]
     # where target is sys.prefix/<target>
-    data_files = find_examples('examples'),
+    data_files = [],
 
     # non zip-safe (never tested it)
     zip_safe = False,
